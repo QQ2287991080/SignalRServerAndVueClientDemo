d5cd75572b04237d99d84bae49fa4a4b721cbed9
Zero <48544629+QQ2287991080@users.noreply.github.com>
2020-09-21 23:29:14 +0800

    312312

--- VueClient/samples-vue/src/views/Home.vue
+++ VueClient/samples-vue/src/views/Home.vue
@@ -6,6 +6,7 @@
     <button @click="sendAll">发送全部</button>
     <button @click="sendOwn">对自己发送</button>
     <button @click="sendClient">系统发送消息</button>
+    <button @click="getLog">获取系统日志</button>
 
     <div>
       <ul v-for="(item ,index) in messages" v-bind:key="index +'itemMessage'">
@@ -22,59 +23,67 @@ import * as signalR from "@aspnet/signalr";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
   },
   data() {
     return {
       user: "", //用户
       message: "", //消息
       connection: "", //signalr连接
-      messages: [] //返回消息
+      messages: [], //返回消息
     };
   },
   methods: {
     //给全部发送消息
-    sendAll: function() {
+    sendAll: function () {
       this.connection
         .invoke("SendMessage", this.user, this.message)
-        .catch(function(err) {
+        .catch(function (err) {
           return console.error(err);
         });
     },
     //只给自己发送消息
-    sendOwn: function() {
+    sendOwn: function () {
       this.connection
         .invoke("SendMessageCaller", this.message)
-        .catch(function(err) {
+        .catch(function (err) {
           return console.error(err);
         });
     },
     //系统发送消息
-    sendClient: function() {
-      this.$http.get("http://localhost:13989/api/test/get").then(resp => {
+    sendClient: function () {
+      this.$http.get("http://localhost:13989/api/test/get").then((resp) => {
         console.log(resp);
       });
-    }
+    },
+    //获取系统日志
+    getLog: function () {
+      this.$http
+        .get("http://localhost:13989/api/test/getLogMessage")
+        .then((res) => {
+          console.log(res);
+        });
+    },
   },
-  created: function() {
+  created: function () {
     let thisVue = this;
     this.connection = new signalR.HubConnectionBuilder()
       .withUrl("http://localhost:13989/chathub", {
         skipNegotiation: true,
-        transport: signalR.HttpTransportType.WebSockets
+        transport: signalR.HttpTransportType.WebSockets,
       })
       .configureLogging(signalR.LogLevel.Information)
       .build();
-    this.connection.on("ReceiveMessage", function(user, message) {
+    this.connection.on("ReceiveMessage", function (user, message) {
       thisVue.messages.push({ user, message });
       console.log({ user, message });
     });
-    this.connection.on("ReceiveCaller", function(message) {
+    this.connection.on("ReceiveCaller", function (message) {
       let user = "自己"; //这里为了push不报错，我就弄了一个默认值。
       thisVue.messages.push({ user, message });
       console.log({ user, message });
     });
     this.connection.start();
-  }
+  },
 };
 </script>
